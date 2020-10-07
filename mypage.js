354a35b52e757f3f335ba6870af72cdde38423cc
Bensille Bruce Aburiyna <bensille.aburinya@amalitech.org>
2020-10-07 10:18:31 +0000

    mypage.js

---
+++ mypage.js
@@ -0,0 +1,14 @@
+let elements = document.getElementsByName('cssProperty');
+let div = document.getElementById('modify');
+
+function set()
+{
+	for( let index = 0; index < elements.length; index++)
+	{
+		let cssProperty = elements[ index ].getAttribute('id');
+		let cssValue = elements[ index ].value;
+		div.style[cssProperty] = cssValue;
+	}
+}
+
+document.getElementById('set').addEventListene
